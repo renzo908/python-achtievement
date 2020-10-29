9a130531ebf9ce21c618431d34134aacb8ac683c
renzo908 <70953384+renzo908@users.noreply.github.com>
2020-10-29 12:25:26 +0100

    Add files via upload

---
+++ lichten.py
@@ -0,0 +1,16 @@
+print ( "wat doe je in een nieuwe klas?" )
+keuze  =  input ()
+if  choice  ==  'praten' :
+    print ( "pak je eerst je laptop of je boeken?" )
+elif  choice  ==  'laptop' :
+    print ( "is voetbal leuker dan zwemmen" )
+else :
+    print ( choice, "dat klopt helemaal" )
+
+print ( "is het friet of patat?" )
+keuze  =  input ()
+if  choice  ==  'patat' :
+    print ( "vindt ik deze opleiding leuk ?" )
+elif  choice  ==  'nee' :
+    
+
